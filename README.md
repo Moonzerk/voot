c248ea64c33ab4c32304f3ff3242972fbb765608
Alex LIGONY <alex.ligony@gmail.com>
2023-05-27 02:59:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Voot
+
+- Vue 3
+- Typescript
+- UnoCss with the TailwindCss preset
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-full bg-gray-900 text-gray-300">
+    Template V00T
+  </div>
+</template>
+
+<script setup lang="ts">
+// something...
+</script>
