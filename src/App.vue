5b63f56a035ed430100e7bc111c62bfb832ed45d
Alex Ligony <35448379+Moonzerk@users.noreply.github.com>
2023-05-27 01:32:05 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="min-h-full bg-gray-900 text-gray-300">
-    Template V00T
+  <div class="min-h-screen bg-gray-900 text-gray-400">
+    <h1 class="p-4 text-xl font-bold">Template V00T</h1>
   </div>
 </template>
 
-<script setup lang="ts">
-// something...
-</script>
+<script setup lang="ts"></script>
